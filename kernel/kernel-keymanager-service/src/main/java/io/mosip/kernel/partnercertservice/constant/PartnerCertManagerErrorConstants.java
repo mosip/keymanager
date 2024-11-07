f1faaea8636f1411009f14ae0019e2e1ddac5612
nagendra0721 <nagendra0718@gmail.com>
2024-11-07 19:05:20 +0530

    MOSIP-36642: Different error code and messages and unable to upload less than a year
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>

--- kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/partnercertservice/constant/PartnerCertManagerErrorConstants.java
+++ kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/partnercertservice/constant/PartnerCertManagerErrorConstants.java
@@ -40,6 +40,8 @@ public enum PartnerCertManagerErrorConstants {
 	SELF_SIGNED_CERT_NOT_ALLOWED("KER-PCM-015", "Self Signed Certificate not allowed as partner."),
 
 	SIGN_CERT_NOT_ALLOWED("KER-PCM-016", "Sign Certificate not allowed for the authenticated token."),
+
+	CERT_VALIDITY_LESS_THAN_MIN_VALIDITY_NOT_ALLOWED("KER-PCM-017","The CA Certificate validity is less than required minimum validity."),
     ;
 
 	/**
