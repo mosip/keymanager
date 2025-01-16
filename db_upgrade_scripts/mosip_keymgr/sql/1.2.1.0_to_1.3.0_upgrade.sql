d7a8d9908bf0f06d9eb6877103fc731aa97a11a2
nagendra0721 <nagendra0718@gmail.com>
2025-01-16 19:10:50 +0530

    renaming file and add role
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>

--- db_upgrade_scripts/mosip_keymgr/sql/1.2.1.0_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_keymgr/sql/1.2.1.0_to_1.3.0_upgrade.sql
@@ -4,4 +4,4 @@ ALTER TABLE IF EXISTS keymgr.ca_cert_store
     ADD COLUMN ca_cert_type character varying(25);
 
 COMMENT ON COLUMN keymgr.ca_cert_store.ca_cert_type
-    IS 'CA_Certificate Type: Specifies the type of CA_Certificate e.g., Root, Intermediate, end-entity';
+    IS 'CA_Certificate Type: Specifies the type of CA_Certificate e.g., Root, Intermediate';
