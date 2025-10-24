48aab48c662ce4ecca1ec64c9200ea2d5702c0e3
nagendra0721 <nagendra0718@gmail.com>
2025-10-24 19:49:19 +0530

    INJICERT-1249: update certificate thumbprint
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>

--- kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/signature/service/impl/SignatureServiceImpl.java
+++ kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/signature/service/impl/SignatureServiceImpl.java
@@ -1118,7 +1118,6 @@ public class SignatureServiceImpl implements SignatureService, SignatureServicev
 		if (trustCertData == null)
 			return SignatureConstant.TRUST_NOT_VERIFIED;
 
-        String certThumbprint = cryptomanagerUtil.getCertificateThumbprintInHex(trustCertData);
 		boolean isTrustValid = partnerCertManagerService.validateCertificatePathWithInterCertTrust(trustCertData, domain, intermediateCerts);
 		if (isTrustValid) {
             LOGGER.info(SignatureConstant.SESSIONID, SignatureConstant.JWT_SIGN, SignatureConstant.BLANK,
@@ -1126,6 +1125,7 @@ public class SignatureServiceImpl implements SignatureService, SignatureServicev
 			return SignatureConstant.TRUST_VALID;
 		}
 
+        String certThumbprint = cryptomanagerUtil.getCertificateThumbprintInHex(trustCertData);
 		LOGGER.info(SignatureConstant.SESSIONID, SignatureConstant.JWT_SIGN, SignatureConstant.BLANK,
 				"JWT Signature Verification Request - Trust Validation - Failed. Certificate Thumbprint: " + certThumbprint);
 		return SignatureConstant.TRUST_NOT_VALID;
