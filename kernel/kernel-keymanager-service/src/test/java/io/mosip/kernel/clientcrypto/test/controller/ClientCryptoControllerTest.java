21fef56d779032470b61c78fd8d3c78cf44fab7f
nagendra0721 <nagendra0718@gmail.com>
2025-10-27 18:21:53 +0530

    MOSIP-30573: add test update for client cryptomanager
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>

--- kernel/kernel-keymanager-service/src/test/java/io/mosip/kernel/clientcrypto/test/controller/ClientCryptoControllerTest.java
+++ kernel/kernel-keymanager-service/src/test/java/io/mosip/kernel/clientcrypto/test/controller/ClientCryptoControllerTest.java
@@ -9,7 +9,6 @@ import io.mosip.kernel.clientcrypto.constant.ClientType;
 import io.mosip.kernel.clientcrypto.controller.ClientCryptoController;
 import io.mosip.kernel.clientcrypto.dto.*;
 import io.mosip.kernel.clientcrypto.service.impl.ClientCryptoFacade;
-import io.mosip.kernel.clientcrypto.service.spi.ClientCryptoManagerService;
 import io.mosip.kernel.clientcrypto.service.spi.ClientCryptoService;
 import io.mosip.kernel.clientcrypto.test.ClientCryptoTestBootApplication;
 import io.mosip.kernel.core.http.RequestWrapper;
@@ -24,14 +23,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PublicKey;
+import java.time.LocalDateTime;
 
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 @SpringBootTest(classes = { ClientCryptoTestBootApplication.class })
 @RunWith(SpringRunner.class)
@@ -44,9 +46,6 @@ public class ClientCryptoControllerTest {
 
     @Autowired
     private ClientCryptoController clientCryptoController;
-
-    @Autowired
-    private ClientCryptoManagerService clientCryptoManagerService;
 
     @Autowired
     private ClientCryptoFacade clientCryptoFacade;
@@ -75,175 +74,168 @@ public class ClientCryptoControllerTest {
     }
 
     @Test
-    public void testSignData_Success() {
+    public void testSignData_Forbidden() throws Exception {
         RequestWrapper<TpmSignRequestDto> request = new RequestWrapper<>();
+        request.setId(ID);
+        request.setVersion(VERSION);
+        request.setRequesttime(LocalDateTime.now());
         TpmSignRequestDto requestDto = new TpmSignRequestDto();
         requestDto.setData(CryptoUtil.encodeToURLSafeBase64(testData));
         request.setRequest(requestDto);
 
-        ResponseWrapper<TpmSignResponseDto> result = clientCryptoController.signData(request);
-
-        assertNotNull(result);
-        assertNotNull(result.getResponse());
-        assertNotNull(result.getResponse().getData());
-        assertFalse(result.getResponse().getData().isEmpty());
+        mockMvc.perform(post("/cssign")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request)))
+                .andExpect(status().isForbidden());
     }
 
     @Test
-    public void testVerifySignature_Success() {
+    @WithUserDetails("test")
+    public void testSignDataMvc_Success() throws Exception {
+        RequestWrapper<TpmSignRequestDto> request = new RequestWrapper<>();
+        request.setId(ID);
+        request.setVersion(VERSION);
+        request.setRequesttime(LocalDateTime.now());
+        TpmSignRequestDto requestDto = new TpmSignRequestDto();
+        requestDto.setData(CryptoUtil.encodeToURLSafeBase64(testData));
+        request.setRequest(requestDto);
+
+        mockMvc.perform(post("/cssign")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request))
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.response.data").isNotEmpty());
+    }
+
+    @Test
+    @WithUserDetails("test")
+    public void testVerifySignatureMvc_Success() throws Exception {
+        // First sign the data
         RequestWrapper<TpmSignRequestDto> signRequest = new RequestWrapper<>();
+        signRequest.setId(ID);
+        signRequest.setVersion(VERSION);
+        signRequest.setRequesttime(LocalDateTime.now());
         TpmSignRequestDto signRequestDto = new TpmSignRequestDto();
         signRequestDto.setData(CryptoUtil.encodeToURLSafeBase64(testData));
         signRequest.setRequest(signRequestDto);
         ResponseWrapper<TpmSignResponseDto> signResult = clientCryptoController.signData(signRequest);
 
+        // Now verify the signature
         RequestWrapper<TpmSignVerifyRequestDto> request = new RequestWrapper<>();
+        request.setId(ID);
+        request.setVersion(VERSION);
+        request.setRequesttime(LocalDateTime.now());
         TpmSignVerifyRequestDto requestDto = new TpmSignVerifyRequestDto();
         requestDto.setData(CryptoUtil.encodeToURLSafeBase64(testData));
         requestDto.setSignature(signResult.getResponse().getData());
 
         ClientCryptoService clientCryptoService = clientCryptoFacade.getClientSecurity();
-        assertNotNull(clientCryptoService);
         byte[] signingPublicKey = clientCryptoService.getSigningPublicPart();
         requestDto.setPublicKey(CryptoUtil.encodeToURLSafeBase64(signingPublicKey));
         requestDto.setClientType(ClientType.LOCAL);
         request.setRequest(requestDto);
 
-        ResponseWrapper<TpmSignVerifyResponseDto> result = clientCryptoController.verifySignature(request);
-
-        assertNotNull(result);
-        assertNotNull(result.getResponse());
-        assertTrue(result.getResponse().isVerified());
-    }
-
-    @Test(expected = io.mosip.kernel.clientcrypto.exception.ClientCryptoException.class)
-    public void testVerifySignature_WithNullClientType() {
-        RequestWrapper<TpmSignVerifyRequestDto> request = new RequestWrapper<>();
-        TpmSignVerifyRequestDto requestDto = new TpmSignVerifyRequestDto();
-        requestDto.setData(CryptoUtil.encodeToURLSafeBase64(testData));
-        requestDto.setSignature(CryptoUtil.encodeToURLSafeBase64("test signature".getBytes()));
-        requestDto.setPublicKey(CryptoUtil.encodeToURLSafeBase64(testPublicKey.getEncoded()));
-        requestDto.setClientType(null);
-        request.setRequest(requestDto);
-        clientCryptoController.verifySignature(request);
+        mockMvc.perform(post("/csverifysign")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request))
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.response.verified").value(true));
     }
 
     @Test
-    public void testTpmEncrypt_Success() {
+    @WithUserDetails("test")
+    public void testTpmEncryptMvc_Success() throws Exception {
         RequestWrapper<TpmCryptoRequestDto> request = new RequestWrapper<>();
+        request.setId(ID);
+        request.setVersion(VERSION);
+        request.setRequesttime(LocalDateTime.now());
         TpmCryptoRequestDto requestDto = new TpmCryptoRequestDto();
         requestDto.setValue(CryptoUtil.encodeToURLSafeBase64(testData));
         requestDto.setPublicKey(CryptoUtil.encodeToURLSafeBase64(testPublicKey.getEncoded()));
         requestDto.setClientType(ClientType.LOCAL);
         request.setRequest(requestDto);
 
-        ResponseWrapper<TpmCryptoResponseDto> result = clientCryptoController.tpmEncrypt(request);
-
-        assertNotNull(result);
-        assertNotNull(result.getResponse());
-        assertNotNull(result.getResponse().getValue());
-        assertFalse(result.getResponse().getValue().isEmpty());
+        mockMvc.perform(post("/tpmencrypt")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request))
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.response.value").isNotEmpty());
     }
 
     @Test
-    public void testTpmEncrypt_WithAndroidClientType() {
-        RequestWrapper<TpmCryptoRequestDto> request = new RequestWrapper<>();
-        TpmCryptoRequestDto requestDto = new TpmCryptoRequestDto();
-        requestDto.setValue(CryptoUtil.encodeToURLSafeBase64(testData));
-        requestDto.setPublicKey(CryptoUtil.encodeToURLSafeBase64(testPublicKey.getEncoded()));
-        requestDto.setClientType(ClientType.ANDROID);
-        request.setRequest(requestDto);
-
-        ResponseWrapper<TpmCryptoResponseDto> result = clientCryptoController.tpmEncrypt(request);
-
-        assertNotNull(result);
-        assertNotNull(result.getResponse());
-        assertNotNull(result.getResponse().getValue());
-        assertFalse(result.getResponse().getValue().isEmpty());
-    }
-
-    @Test
-    public void testTpmEncrypt_WithNullClientType() {
-        RequestWrapper<TpmCryptoRequestDto> request = new RequestWrapper<>();
-        TpmCryptoRequestDto requestDto = new TpmCryptoRequestDto();
-        requestDto.setValue(CryptoUtil.encodeToURLSafeBase64(testData));
-        requestDto.setPublicKey(CryptoUtil.encodeToURLSafeBase64(testPublicKey.getEncoded()));
-        requestDto.setClientType(null);
-        request.setRequest(requestDto);
-
-        ResponseWrapper<TpmCryptoResponseDto> result = clientCryptoController.tpmEncrypt(request);
-
-        assertNotNull(result);
-        assertNotNull(result.getResponse());
-        assertNotNull(result.getResponse().getValue());
-        assertFalse(result.getResponse().getValue().isEmpty());
-    }
-
-    @Test
-    public void testTpmDecrypt_Success() {
+    @WithUserDetails("test")
+    public void testTpmDecryptMvc_Success() throws Exception {
+        // First encrypt the data
         RequestWrapper<TpmCryptoRequestDto> encryptRequest = new RequestWrapper<>();
+        encryptRequest.setId(ID);
+        encryptRequest.setVersion(VERSION);
+        encryptRequest.setRequesttime(LocalDateTime.now());
         TpmCryptoRequestDto encryptRequestDto = new TpmCryptoRequestDto();
         encryptRequestDto.setValue(CryptoUtil.encodeToURLSafeBase64(testData));
 
         ClientCryptoService clientCryptoService = clientCryptoFacade.getClientSecurity();
-        assertNotNull(clientCryptoService);
         byte[] encryptionPublicKey = clientCryptoService.getEncryptionPublicPart();
         encryptRequestDto.setPublicKey(CryptoUtil.encodeToURLSafeBase64(encryptionPublicKey));
         encryptRequestDto.setClientType(null);
         encryptRequest.setRequest(encryptRequestDto);
         ResponseWrapper<TpmCryptoResponseDto> encryptResult = clientCryptoController.tpmEncrypt(encryptRequest);
 
+        // Now decrypt the data
         RequestWrapper<TpmCryptoRequestDto> request = new RequestWrapper<>();
+        request.setId(ID);
+        request.setVersion(VERSION);
+        request.setRequesttime(LocalDateTime.now());
         TpmCryptoRequestDto requestDto = new TpmCryptoRequestDto();
         requestDto.setValue(encryptResult.getResponse().getValue());
+        requestDto.setPublicKey(CryptoUtil.encodeToURLSafeBase64(encryptionPublicKey));
         request.setRequest(requestDto);
 
-        ResponseWrapper<TpmCryptoResponseDto> result = clientCryptoController.tpmDecrypt(request);
-
-        assertNotNull(result);
-        assertNotNull(result.getResponse());
-        assertNotNull(result.getResponse().getValue());
+        mockMvc.perform(post("/tpmdecrypt")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request))
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.response.value").isNotEmpty());
     }
 
-
     @Test
-    public void testGetSigningPublicKey_Success() {
+    @WithUserDetails("test")
+    public void testGetSigningPublicKeyMvc_Success() throws Exception {
         RequestWrapper<PublicKeyRequestDto> request = new RequestWrapper<>();
+        request.setId(ID);
+        request.setVersion(VERSION);
+        request.setRequesttime(LocalDateTime.now());
         PublicKeyRequestDto requestDto = new PublicKeyRequestDto();
+        requestDto.setServerProfile("test");
         request.setRequest(requestDto);
 
-        ResponseWrapper<PublicKeyResponseDto> result = clientCryptoController.getSigningPublicKey(request);
-
-        assertNotNull(result);
-        assertNotNull(result.getResponse());
-        assertNotNull(result.getResponse().getPublicKey());
-        assertFalse(result.getResponse().getPublicKey().isEmpty());
+        mockMvc.perform(post("/tpmsigning/publickey")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request))
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.response.publicKey").isNotEmpty());
     }
 
     @Test
-    public void testGetEncPublicKey_Success() {
+    @WithUserDetails("test")
+    public void testGetEncPublicKeyMvc_Success() throws Exception {
         RequestWrapper<PublicKeyRequestDto> request = new RequestWrapper<>();
+        request.setId(ID);
+        request.setVersion(VERSION);
+        request.setRequesttime(LocalDateTime.now());
         PublicKeyRequestDto requestDto = new PublicKeyRequestDto();
+        requestDto.setServerProfile("test");
         request.setRequest(requestDto);
 
-        ResponseWrapper<PublicKeyResponseDto> result = clientCryptoController.getEncPublicKey(request);
-
-        assertNotNull(result);
-        assertNotNull(result.getResponse());
-        assertNotNull(result.getResponse().getPublicKey());
-        assertFalse(result.getResponse().getPublicKey().isEmpty());
-    }
-
-    @Test
-    public void testSignData_Forbidden() throws Exception {
-        RequestWrapper<TpmSignRequestDto> request = new RequestWrapper<>();
-        TpmSignRequestDto requestDto = new TpmSignRequestDto();
-        requestDto.setData(CryptoUtil.encodeToURLSafeBase64(testData));
-        request.setRequest(requestDto);
-
-        mockMvc.perform(post("/cssign")
-                        .contentType("text/plain")
-                        .content(mapper.writeValueAsString(request)))
-                .andExpect(status().isForbidden());
+        mockMvc.perform(post("/tpmencryption/publickey")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request))
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.response.publicKey").isNotEmpty());
     }
 }
