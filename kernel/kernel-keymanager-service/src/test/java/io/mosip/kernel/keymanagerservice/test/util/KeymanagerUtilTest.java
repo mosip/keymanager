ed381c09f2b118ed3bbc58b1cc514494e40d37a3
nagendra0721 <nagendra0718@gmail.com>
2025-11-11 16:25:42 +0530

    MOSIP-30573: sonar coverage for release branch
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>

--- kernel/kernel-keymanager-service/src/test/java/io/mosip/kernel/keymanagerservice/test/util/KeymanagerUtilTest.java
+++ kernel/kernel-keymanager-service/src/test/java/io/mosip/kernel/keymanagerservice/test/util/KeymanagerUtilTest.java
@@ -10,6 +10,8 @@ import java.security.PrivateKey;
 import java.security.Security;
 import java.security.cert.X509Certificate;
 import java.time.LocalDateTime;
+
+import io.mosip.kernel.keymanagerservice.exception.KeymanagerServiceException;
 import io.mosip.kernel.keymanagerservice.validator.ECKeyPairGenRequestValidator;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.Before;
@@ -287,4 +289,23 @@ public class KeymanagerUtilTest {
 	public void testPurgeKeyAliasTrustAnchorsCache() {
 		keymanagerUtil.purgeKeyAliasTrustAnchorsCache();
 	}
+
+    @Test(expected = KeymanagerServiceException.class)
+    public void testConvertToCertificateKeymanagerServiceException() {
+        keymanagerUtil.convertToCertificate("INVALID_CERT_DATA");
+        keymanagerUtil.convertToCertificate((byte[]) null);
+    }
+
+    @Test(expected = KeymanagerServiceException.class)
+    public void testGetPEMFormatedDataKeymanagerServiceException() {
+        keymanagerUtil.getPEMFormatedData("CERTIFICATE_DATA_INVALID");
+    }
+
+    @Test
+    public void testDestroySecreteKey() throws NoSuchAlgorithmException {
+        javax.crypto.KeyGenerator keyGenerator = javax.crypto.KeyGenerator.getInstance("AES");
+        keyGenerator.init(256);
+        javax.crypto.SecretKey secretKey = keyGenerator.generateKey();
+        keymanagerUtil.destoryKey(secretKey);
+    }
 }
