3fa0a5ba62781714458d178fbf89b2003dad8a0e
tarique-azeez <mdtarique2703@gmail.com>
2025-11-24 19:09:41 +0530

    remove duplicate rollback script (#485)
    Signed-off-by: tarique-azeez <mdtarique2703@gmail.com>

--- db_upgrade_scripts/mosip_keymgr/sql/1.2.1.0_to_1.3.0_rollback.sql
+++ db_upgrade_scripts/mosip_keymgr/sql/1.2.1.0_to_1.3.0_rollback.sql
@@ -8,12 +8,3 @@ COMMENT ON COLUMN keymgr.ca_cert_store.ca_cert_type
 ALTER TABLE IF EXISTS keymgr.ca_cert_store
     DROP COLUMN IF EXISTS ca_cert_type;
 
--- Below script is required to rollback from 1.3.0-B2 to 1.3.0-B1 --
--- ca_cert_type column is removed/deleted from ca_cert_store table --
-\c mosip_keymgr
-
-COMMENT ON COLUMN keymgr.ca_cert_store.ca_cert_type
-    IS NULL;
-
-ALTER TABLE IF EXISTS keymgr.ca_cert_store
-    DROP COLUMN IF EXISTS ca_cert_type;
