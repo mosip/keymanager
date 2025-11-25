880b0a75a9cc51ab74d433da32e15d5adcdc78b5
nagendra0721 <nagendra0718@gmail.com>
2025-11-25 14:50:26 +0530

    MOSIP-30573: key generator service test case for release-1.3.x
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>

--- kernel/kernel-keymanager-service/src/test/java/io/mosip/kernel/keygenerator/bouncycastle/test/KeyGeneratorTest.java
+++ kernel/kernel-keymanager-service/src/test/java/io/mosip/kernel/keygenerator/bouncycastle/test/KeyGeneratorTest.java
@@ -1,12 +1,16 @@
 package io.mosip.kernel.keygenerator.bouncycastle.test;
 
 import static org.hamcrest.CoreMatchers.isA;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
 
 import javax.crypto.SecretKey;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +20,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import io.mosip.kernel.core.keymanager.spi.ECKeyStore;
 import io.mosip.kernel.keygenerator.bouncycastle.KeyGenerator;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -27,6 +32,11 @@ public class KeyGeneratorTest {
 	@MockBean
 	private ECKeyStore keyStore;
 
+    @Before
+    public void init() {
+        ReflectionTestUtils.setField(keyGenerator, "secureRandom", null);
+    }
+
 	@Test
 	public void testGetSymmetricKey() {
 		assertThat(keyGenerator.getSymmetricKey(), isA(SecretKey.class));
@@ -35,7 +45,61 @@ public class KeyGeneratorTest {
 	@Test
 	public void testGetAsymmetricKey() {
 		assertThat(keyGenerator.getAsymmetricKey(), isA(KeyPair.class));
-
 	}
 
+    @Test
+    public void getSymmetricKeyTest() {
+        SecretKey key = keyGenerator.getSymmetricKey();
+        assertThat(key, isA(SecretKey.class));
+        assertNotNull(key.getEncoded());
+        assertTrue(key.getEncoded().length > 0);
+    }
+
+    @Test
+    public void getAsymmetricKeyTest() {
+        KeyPair keyPair = keyGenerator.getAsymmetricKey();
+        assertThat(keyPair, isA(KeyPair.class));
+        assertNotNull(keyPair.getPublic());
+        assertNotNull(keyPair.getPrivate());
+    }
+
+    @Test
+    public void testBuildPrivateKey() {
+        KeyPair keyPair = keyGenerator.getEd25519KeyPair();
+        byte[] privateKeyData = keyPair.getPrivate().getEncoded();
+        PrivateKey rebuiltKey = keyGenerator.buildPrivateKey(privateKeyData);
+        assertNotNull(rebuiltKey);
+    }
+
+    @Test
+    public void testSecureRandomCaching() {
+        ReflectionTestUtils.setField(keyGenerator, "secureRandom", new SecureRandom());
+        SecureRandom result = (SecureRandom) ReflectionTestUtils.invokeMethod(keyGenerator, "getSecureRandom");
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testSecureRandomRngDisabled() {
+        ReflectionTestUtils.setField(keyGenerator, "rngProviderEnabled", false);
+        SecureRandom result = (SecureRandom) ReflectionTestUtils.invokeMethod(keyGenerator, "getSecureRandom");
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testSecureRandomRngEnabled() {
+        when(keyStore.getKeystoreProviderName()).thenReturn("SUN");
+        ReflectionTestUtils.setField(keyGenerator, "rngProviderEnabled", true);
+        ReflectionTestUtils.setField(keyGenerator, "rngProviderName", "SHA1PRNG");
+        SecureRandom result = (SecureRandom) ReflectionTestUtils.invokeMethod(keyGenerator, "getSecureRandom");
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testSecureRandomFallback() {
+        when(keyStore.getKeystoreProviderName()).thenReturn("SUN");
+        ReflectionTestUtils.setField(keyGenerator, "rngProviderEnabled", true);
+        ReflectionTestUtils.setField(keyGenerator, "rngProviderName", "INVALID_PROVIDER");
+        SecureRandom result = (SecureRandom) ReflectionTestUtils.invokeMethod(keyGenerator, "getSecureRandom");
+        assertNotNull(result);
+    }
 }
