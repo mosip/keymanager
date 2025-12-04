abd1952f117308db923962db1920523672cc2247
Nandhukumar <nandhukumare@gmail.com>
2025-12-04 20:44:40 +0530

    MOSIP-43617 | added missed indexes
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>

--- db_upgrade_scripts/mosip_keymgr/sql/1.2.1.0_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_keymgr/sql/1.2.1.0_to_1.3.0_upgrade.sql
@@ -6,3 +6,11 @@ ALTER TABLE IF EXISTS keymgr.ca_cert_store
 
 COMMENT ON COLUMN keymgr.ca_cert_store.ca_cert_type
     IS 'CA_Certificate Type: Specifies the type of CA_Certificate e.g., Root, Intermediate';
+
+
+--PERFORMANCE INDEXES--
+CREATE INDEX IF NOT EXISTS idx_ca_cert_store_cr_dtimes ON keymgr.ca_cert_store USING btree (cr_dtimes);
+CREATE INDEX IF NOT EXISTS idx_ca_cert_store_del_dtimes ON keymgr.ca_cert_store USING btree (del_dtimes);
+CREATE INDEX IF NOT EXISTS idx_ca_cert_store_upd_dtimes ON keymgr.ca_cert_store USING btree (upd_dtimes);
+CREATE INDEX IF NOT EXISTS idx_ca_cert_times ON keymgr.ca_cert_store USING btree (cr_dtimes, upd_dtimes, del_dtimes);
+--END PERFORMANCE INDEXES--
